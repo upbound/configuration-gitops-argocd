2d47caa1a1005bb91c1ba0211c9c1e17e90a6fb7
Yury Tsarev <yury@upbound.io>
2025-10-16 15:19:00 +0200

    Migration to Upbound Devex
    Signed-off-by: Yury Tsarev <yury@upbound.io>

--- project.mk
+++
@@ -1,4 +0,0 @@
-PROJECT_NAME := configuration-gitops-argocd
-UPTEST_INPUT_MANIFESTS := examples/argocd-xr.yaml
-UPTEST_SKIP_UPDATE := true
-XPKG_IGNORE ?= .github/workflows/*.yaml,.github/workflows/*.yml,examples/*.yaml,.work/uptest-datasource.yaml,.cache/render/*,test/provider/*,gitops/*
---
+++ tests/test-xargo/kcl.mod
@@ -0,0 +1,6 @@
+[package]
+name = "test-xargo"
+version = "0.0.1"
+
+[dependencies]
+models = { path = "./model" }
